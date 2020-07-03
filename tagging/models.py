f6e08c39fcd156aa64eb167ed80ef324f508a7b8
francisco souza <fsouza@users.noreply.github.com>
2020-07-03 19:21:57 -0400

    models: fix GenericForeignKey usage

--- tagging/models.py
+++ tagging/models.py
@@ -4,7 +4,7 @@ Models and managers for generic tagging.
 
 from __future__ import absolute_import
 
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection
 from django.db import models
@@ -538,7 +538,7 @@ class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, verbose_name=_("tag"), related_name="items")
     content_type = models.ForeignKey(ContentType, verbose_name=_("content type"))
     object_id = models.PositiveIntegerField(_("object id"), db_index=True)
-    object = generic.GenericForeignKey("content_type", "object_id")
+    object = GenericForeignKey("content_type", "object_id")
 
     objects = TaggedItemManager()
 
